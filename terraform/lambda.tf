984cd32bcf90895ec3395bc30c76027ef80a310a
forestnd <10146999+forestnd@users.noreply.github.com>
2026-02-08 22:21:48 +0000

    Renamed lambda layer to match project change

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -18,7 +18,7 @@ resource "aws_lambda_function" "lotochecker_function" {
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   timeout          = 30
   layers = [
-    "arn:aws:lambda:${data.aws_region.current.name}:519388350760:layer:awssetup-dub-lambda-layer-requests:2",
+    "arn:aws:lambda:${data.aws_region.current.name}:519388350760:layer:awsenv-dub-lambda-layer-requests:1",
   ]
   environment {
     variables = {
